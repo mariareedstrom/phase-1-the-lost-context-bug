8e58c159db98d4762cf3f11f5180e3d2331374a1
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-11-29 16:26:32 -0600

    commit tree fac00dde8bcd929635ef5bd2871f648d6b0533b0

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const messageConfig = {
   frontContent: "Happy Birthday, Odin One-Eye!",
-  insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
+  insideContent:
+    "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
   closing: {
-      "Thor": "Admiration, respect, and love",
-      "Loki": "Your son"
+    Thor: "Admiration, respect, and love",
+    Loki: "Your son",
   },
-  signatories: [
-      "Thor",
-      "Loki"
-  ]
+  signatories: ["Thor", "Loki"],
 };
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
